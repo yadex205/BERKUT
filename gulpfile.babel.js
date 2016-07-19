43ab4abe2253cd6c121d1876d8444aeb0b234638
Yadex205 <yadex205@outlook.jp>
2016-07-19 23:53:13 +0900

    :wrench: setup develop environment

---
+++ gulpfile.babel.js
@@ -0,0 +1,85 @@
+import gulp from 'gulp'
+import ejs from 'gulp-ejs'
+import sass from 'gulp-sass'
+import plumber from 'gulp-plumber'
+import notify from 'gulp-notify'
+import livereload from 'gulp-livereload'
+import concat from 'gulp-concat'
+import sourcemaps from 'gulp-sourcemaps'
+import electron from 'electron-prebuilt'
+import rimraf from 'rimraf'
+
+var spawn = require('child_process').spawn
+
+var targetDir = 'htdocs'
+var plumberOptions = {
+    errorHandler: notify.onError('Error <%= error.message %>')
+}
+
+gulp.task('default')
+
+gulp.task('build', ['html', 'css', 'js'])
+
+gulp.task('live', ['html_live', 'css', 'js'], () => {
+    livereload.listen()
+    let app = spawn(electron, ['.'])
+
+    gulp.watch('src/html/**/*.ejs', ['html_live'])
+    gulp.watch('src/css/**/*.scss', ['css'])
+    gulp.watch('src/js/**/*.js', ['js'])
+
+    app.on('close', () => {
+        process.exit(1)
+    })
+})
+
+gulp.task('clean', (callback) => {
+    rimraf(targetDir, callback)
+})
+
+gulp.task('html', () => {
+    gulp.src(['src/html/**/*.ejs', '!src/html/**/_*.ejs'])
+        .pipe(plumber(plumberOptions))
+        .pipe(ejs({
+            isLive: false
+        }, {
+            ext: '.html'
+        }))
+        .pipe(gulp.dest(targetDir))
+        .pipe(livereload())
+})
+
+gulp.task('html_live', () => {
+    gulp.src(['src/html/**/*.ejs', '!src/html/**/_*.ejs'])
+        .pipe(plumber(plumberOptions))
+        .pipe(ejs({
+            isLive: true
+        }, {
+            ext: '.html'
+        }))
+        .pipe(gulp.dest(targetDir))
+        .pipe(livereload())
+})
+
+gulp.task('css', () => {
+    gulp.src('src/css/**/*.scss')
+        .pipe(plumber(plumberOptions))
+        .pipe(sass({
+            includePaths: ['bower_components/bootstrap-sass/assets/stylesheets']
+        }))
+        .pipe(gulp.dest(`${targetDir}/css`))
+        .pipe(livereload())
+})
+
+gulp.task('js', () => {
+    gulp.src([
+        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+        'src/js/class/**/*.js',
+        'src/js/*.js'])
+        .pipe(plumber(plumberOptions))
+        .pipe(sourcemaps.init())
+        .pipe(concat('index.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(`${targetDir}/js`))
+        .pipe(livereload())
+})
---
+++ index.js
@@ -0,0 +1,26 @@
+// index.js
+
+const electron = require('electron')
+
+const {app} = electron
+const {BrowserWindow}= electron
+
+let mainWindow
+
+function createWindow () {
+    mainWindow = new BrowserWindow()
+    mainWindow.loadURL(`file://${__dirname}/htdocs/index.html`)
+    app.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    switch(process.platform) {
+    case 'darwin': app.quit()
+    }
+})
+
+app.on('activate', (mainWindow === null) ? createWindow : ()=>{})
