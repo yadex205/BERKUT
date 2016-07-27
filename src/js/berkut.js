808887f27e65ffb0854b0eb7c93ac9da12b771e2
Yadex205 <yadex205@outlook.jp>
2016-07-27 11:31:55 +0900

    :recycle: bind layers to Vue

--- src/js/berkut.js
+++ src/js/berkut.js
@@ -1,14 +1,23 @@
 // berkut.js
 
 var BERKUT = function() {
-    this._bindingRoot = new Vue({
-        el: 'body'
+    document.addEventListener('DOMContentLoaded', () => {
+        this.layers = new BERKUT.Layers()
     })
+}
 
-    this._layers = new Vue({
-        el: '#berkut-layers-holder',
-        data: {
-            layers: new Array(6)
-        }
-    })
+BERKUT.Layers = Vue.extend({
+    el: () => { return '#berkut-layers-holder' },
+    data: () => { return {
+        layers: new Array(6).fill(new BERKUT.Layer())
+    } }
+})
+
+BERKUT.Layer = function() {
+    this.position = 0.0
+    this.duration = 5.0
+    this.blendTechnique = 'normal'
+    this.mute = false
+    this.solo = false
+    this.rhythm = false
 }
