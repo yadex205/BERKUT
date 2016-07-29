974ca6e7e585ba0686b35ddfc2a79ad97e842ca3
Yadex205 <yadex205@outlook.jp>
2016-07-29 21:42:33 +0900

    :bug: bind bootstrap-slider after vue is ready

--- src/js/berkut.js
+++ src/js/berkut.js
@@ -11,7 +11,16 @@ BERKUT.Layers = Vue.extend({
     el: () => { return '#berkut-layers-holder' },
     data: () => { return {
         layers: new Array(6).fill(new BERKUT.Layer())
-    } }
+    } },
+    ready: () => {
+        $('div.layer-controller-seekbar input.seekbar').slider({
+            min: 0, max: 1, step: 0.01, value: 0, tooltip: 'hide'
+        })
+        $('div.opacity-slider input.opacity').slider({
+            min: 0, max: 1, step: 0.01, value: 0, tooltip_position: 'right',
+            orientation: 'vertical', reversed: true, selection: 'after'
+        })
+    }
 })
 
 BERKUT.Layer = function() {
