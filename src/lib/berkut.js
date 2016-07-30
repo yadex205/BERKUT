9314207bdf5b6ed9235fffba3cd2e8ce7229d07e
Yadex205 <yadex205@outlook.jp>
2016-07-30 23:14:41 +0900

    :up: introduce webgl-video-renderer

--- src/lib/berkut.js
+++ src/lib/berkut.js
@@ -1,8 +1,14 @@
 // lib/berkut.js
 
 const WebChimera = require('webchimera.js')
+const Canvas = require('canvas')
 
 
 module.exports = function () {
 	
+}
+
+var Mixer = function () {
+	this.players = []
+	this.canvas = new Canvas()
 }
