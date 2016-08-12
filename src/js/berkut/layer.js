ddabf9d191a7205980da1bf9851d21ddb61d92ca
Yadex205 <yadex205@outlook.jp>
2016-08-12 23:57:06 +0900

    :tada: Add seekbar

--- src/js/berkut/layer.js
+++ src/js/berkut/layer.js
@@ -13,8 +13,12 @@ BERKUT.Deck = Vue.extend({
                     rhythm: false,
                     opacity: 0,
                     blend: 'normal',
+                    time: 0,
+                    duration: 5,
                     _renderer: new I420Renderer(),
-                    _frame: null
+                    _frame: null,
+                    _seekbar: null,
+                    _isSeeking : false
                 }
             }),
             preview: (() => {
@@ -29,7 +33,20 @@ BERKUT.Deck = Vue.extend({
         }
     },
     ready: function () {
-        BERKUT.createSliders('.berkut-layer-seekbar', { tooltip: 'hide' })
+        BERKUT.createSliders('.berkut-layer-seekbar', { tooltip: 'hide' }, (slider, index) => {
+            this.layers[index]._seekbar = slider
+            slider.on('slideStart', () => {
+                this.layers[index]._isSeeking = true
+                EventEmitter.emit(BERKUT.PlayerManager.Event.DO_PAUSE, index)
+            })
+            slider.on('slideStop', () => {
+                this.layers[index]._isSeeking = false
+                EventEmitter.emit(BERKUT.PlayerManager.Event.DO_PLAY, index)
+            })
+            slider.on('change', (event) => {
+                EventEmitter.emit(BERKUT.PlayerManager.Event.SEEK_POS, index, event.value.newValue)
+            })
+        })
         BERKUT.createSliders('.berkut-layer-opacity-slider', {
             tooltip_position: 'right',
             orientation: 'vertical',
@@ -41,17 +58,27 @@ BERKUT.Deck = Vue.extend({
         this.$el.querySelectorAll('.berkut-layer-preview').forEach((view, index) => {
             const renderer = this.layers[index]._renderer
             renderer.bind(view)
-            EventEmitter.on(BERKUT.PlayerManager.Event.FRAME_SETUP, (playerIndex, width, height) => {
-                if (playerIndex === index) { renderer.setSize(width, height) }
+            EventEmitter.on(BERKUT.PlayerManager.Event.FRAME_SETUP, (playerIndex, width, height, player) => {
+                if (playerIndex === index) {
+                    renderer.setSize(width, height)
+                    this.layers[index].duration = player.duration
+                }
             })
-            EventEmitter.on(BERKUT.PlayerManager.Event.FRAME_UPDATED, (playerIndex, time, videoFrame) => {
+            EventEmitter.on(BERKUT.PlayerManager.Event.FRAME_UPDATED, (playerIndex, player, videoFrame) => {
                 if (playerIndex === index) {
                     renderer.draw(videoFrame, 'normal', 1.0)
                     this.layers[index]._frame = videoFrame
+                    this.layers[index].time = player.time
+                    if (!this.layers[index]._isSeeking) {
+                        this.layers[index]._seekbar.slider('setValue', player.time / player.length)
+                    }
                 }
             })
         })
         this.blendTask = setInterval(() => { this.blend() }, 1000 / 30)
+    },
+    watch: {
+        
     },
     methods: {
         play: function (index, filepath) {
