91906dbad9b4ed5b46fbde84a262461092c5ebf9
Yadex205 <yadex205@outlook.jp>
2016-08-13 19:13:29 +0900

    Midi mapping

---
+++ midi/KORG_nanoKontrol2.js
@@ -0,0 +1,11 @@
+const MidiController = require('../lib/midi/controller')
+const Event = require('../lib/event')
+const In = Event.In
+
+module.exports = new MidiController({
+	in: () => {
+		// Deck
+
+		// Blender
+	}
+})
