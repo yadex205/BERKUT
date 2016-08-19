0b27da7f6245fe47f15fe64927f2c444b3073b86
Yadex205 <yadex205@outlook.jp>
2016-08-19 20:07:56 +0900

    automatic midi loader

--- src/js/berkut.js
+++ src/js/berkut.js
@@ -4,5 +4,18 @@ var BERKUT = function() {
         this.midiManager = new MidiManager(EventEmitter)
         this.deck = new BERKUT.Deck()
         this.finder = new BERKUT.Finder()
+
+        this.midiManager.deviceList().forEach((device, index) => {
+            this.midiManager.enableDevice(index)
+        })
+        require('fs').readdir('midi', (err, files) => {
+            files.forEach(file => {
+                try {
+                    this.midiManager.addControllerMap(file)
+                } catch (e) {
+                    console.warn(`${file} is unavailable`)
+                }
+            })
+        })
     })
 }
