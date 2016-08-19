af58f3d7cd1c7e146f14aac888db93b343397e4c
Yadex205 <yadex205@outlook.jp>
2016-08-19 20:08:57 +0900

    reverse opacity order

--- midi/KORG_nanoKontrol2.js
+++ midi/KORG_nanoKontrol2.js
@@ -7,11 +7,11 @@ module.exports = new MidiController({
 		// Deck
 
 		// Blender
-		this.onValue(0xB9, 0x00, In.OPACITY_1)
-		this.onValue(0xB9, 0x01, In.OPACITY_2)
-		this.onValue(0xB9, 0x02, In.OPACITY_3)
-		this.onValue(0xB9, 0x03, In.OPACITY_4)
-		this.onValue(0xB9, 0x04, In.OPACITY_5)
-		this.onValue(0xB9, 0x05, In.OPACITY_6)
+		this.onValue(0xB9, 0x00, In.OPACITY_6)
+		this.onValue(0xB9, 0x01, In.OPACITY_5)
+		this.onValue(0xB9, 0x02, In.OPACITY_4)
+		this.onValue(0xB9, 0x03, In.OPACITY_3)
+		this.onValue(0xB9, 0x04, In.OPACITY_2)
+		this.onValue(0xB9, 0x05, In.OPACITY_1)
 	}
 })
