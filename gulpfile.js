a680912ea81b14cb809de37466845137dbbad0ce
Yadex205 <yadex205@outlook.jp>
2016-08-23 15:33:56 +0900

    :sparkles: Define css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp')
 const ejs = require('gulp-ejs')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
 
 const plumber = require('gulp-plumber')
 const notify = require('gulp-notify')
@@ -12,12 +14,23 @@ const ejsSetting = { ext: '.html' }
 gulp.task('default')
 
 gulp.task('watch', ['html'], () => {
-    gulp.watch('app/view/**/*.ejs', ['html'])
+    gulp.watch('app/views/**/*.ejs', ['html'])
 })
 
 gulp.task('html', () => {
-    gulp.src(['app/view/**/*.ejs', '!app/view/**/_*.ejs'])
+    gulp.src(['app/views/**/*.ejs', '!app/view/**/_*.ejs'])
         .pipe(plumber(plumberOption))
+        .pipe(sourcemaps.init())
         .pipe(ejs({}, ejsSetting))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('htdocs'))
+})
+
+gulp.task('css', () => {
+    gulp.src('app/assets/styles/**/*.s+(a|c)ss')
+        .pipe(plumber(plumberOption))
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('htdocs/assets/styles'))
 })
