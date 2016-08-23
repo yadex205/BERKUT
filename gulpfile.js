6a01914f0d2c401c2ee9b18f3faf9949fafce294
Yadex205 <yadex205@outlook.jp>
2016-08-23 15:15:58 +0900

    :sparkles: Add html task

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp')
+const ejs = require('gulp-ejs')
+
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
+
+const plumberOption = {
+    errorHandler: notify.onError('Error: <%= error.message %>')
+}
+const ejsSetting = { ext: '.html' }
+
+gulp.task('default')
+
+gulp.task('html', () => {
+    gulp.src(['app/view/**/*.ejs', '!app/view/**/_*.ejs'])
+        .pipe(plumber(plumberOption))
+        .pipe(ejs({}, ejsSetting))
+        .pipe(gulp.dest('htdocs'))
+})
