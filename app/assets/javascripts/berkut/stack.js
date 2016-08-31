79777d9402761bbfb8b9c3abce553cf42ea1a1a9
Yadex205 <yadex205@outlook.jp>
2016-08-31 22:27:08 +0900

    :sparkles: Add simple seekbar

--- app/assets/javascripts/berkut/stack.js
+++ app/assets/javascripts/berkut/stack.js
@@ -1,6 +1,14 @@
 BERKUT.Stack = function () {
+  const SEEKBAR_OPTIONS = {
+    min: 0, max: 1, step: 0.01, value: 0, tooltip: 'hide'
+  }
+
   Vue.component('component-layer', Vue.extend({
-    template: '#template-layer'
+    template: '#template-layer',
+    ready: function () {
+      const seekbarFactory = this.$els.seekbarFactory
+      $(seekbarFactory).slider(SEEKBAR_OPTIONS)
+    }
   }))
 
   this._vue = new Vue({
