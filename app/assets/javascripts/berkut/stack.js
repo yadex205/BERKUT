2482bf2df9826fd54c6b285ebb994413136f10fd
Yadex205 <yadex205@outlook.jp>
2016-08-31 22:46:40 +0900

    :sparkles: Add opacity selector

--- app/assets/javascripts/berkut/stack.js
+++ app/assets/javascripts/berkut/stack.js
@@ -1,17 +1,21 @@
 BERKUT.Stack = function () {
-  const SEEKBAR_OPTIONS = {
-    min: 0, max: 1, step: 0.01, value: 0, tooltip: 'hide'
-  }
-
-  Vue.component('component-layer', Vue.extend({
-    template: '#template-layer',
-    ready: function () {
-      const seekbarFactory = this.$els.seekbarFactory
-      $(seekbarFactory).slider(SEEKBAR_OPTIONS)
+    const SEEKBAR_OPTIONS = {
+        min: 0, max: 1, step: 0.01, value: 0, tooltip: 'hide'
     }
-  }))
+    const OPACITY_SELECTOR_OPTIONS = {
+        min: 0, max: 1, step: 0.01, value: 0, tooltip: 'hide',
+        orientation: 'vertical', selection: 'after', reversed: true
+    }
 
-  this._vue = new Vue({
-    el: '#stack'
-  })
+    Vue.component('component-layer', Vue.extend({
+        template: '#template-layer',
+        ready: function () {
+            $(this.$els.seekbarFactory).slider(SEEKBAR_OPTIONS)
+            $(this.$els.opacitySelectorFactory).slider(OPACITY_SELECTOR_OPTIONS)
+        }
+    }))
+
+    this._vue = new Vue({
+        el: '#stack'
+    })
 }
