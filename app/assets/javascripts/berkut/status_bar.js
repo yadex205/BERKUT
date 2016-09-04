7cbae4d2f19f6d99fd333a66691e70155cd29202
Yadex205 <yadex205@outlook.jp>
2016-09-04 20:59:58 +0900

    :+1: Bind vue for meters

--- app/assets/javascripts/berkut/status_bar.js
+++ app/assets/javascripts/berkut/status_bar.js
@@ -8,9 +8,26 @@ BERKUT.StatusBar = function () {
 
     this._vue = new Vue({
         el: '#status-bar',
+        data: {
+            _cpuMeter: null,
+            _gpuMeter: null
+        },
         ready: function () {
-            $(this.$els.cpuMeterFactory).slider(OPTIONS)
-            $(this.$els.gpuMeterFactory).slider(OPTIONS)
+            this._cpuMeter = $(this.$els.cpuMeterFactory).slider(OPTIONS)
+            this._gpuMeter = $(this.$els.gpuMeterFactory).slider(OPTIONS)
+        },
+        events: {
+            'cpu-load-change': function (load) {
+                this._cpuMeter.slider('setValue', load)
+            },
+            'gpu-load-change': function (load) {
+                this._gpuMeter.slider('setValue', load)
+            }
         }
     })
+}
+
+BERKUT.StatusBar.Event = {
+    CPU_LOAD_CHANGE: 'cpu-load-change',
+    GPU_LOAD_CHANGE: 'gpu-load-change'
 }
